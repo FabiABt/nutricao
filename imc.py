61d57a4b5875a0d5d3da706159a2cd10fa75aacb
FabiABt <fabiane489@gmail.com>
2022-12-06 10:23:44 -0300

    alteracao de < e >

--- imc.py
+++ imc.py
@@ -2,26 +2,26 @@
 from math import pow
 peso = float(input(" Digite o peso em kg: "))
 Altura = float(input(" Digite a estatura em metros: "))
-idade = input(" Idade: ")
+idade = float(input(" Idade: "))
 imc = peso/pow(Altura, 2)
 print("O IMC é : {:.2f} ".format(imc))
 if idade >= 65 and imc<= 18.49:
     print(" Classificação nutrcional: Idoso Baixo peso ")
-elif idade >= 65 and 18.5<imc and imc >=24.99:
+elif idade >=65 and imc>= 18.5 and imc<= 24.99:
     print(" Classificação nutrcional: Idoso com peso Adequado ")
-elif idade >= 65 and imc<= 25 and imc >=29.99:
+elif idade >= 65 and imc>= 25 and imc<=29.99:
     print(" Classificação nutrcional: Idoso Sobrepeso ")
-elif idade >= 65 and imc< 30:
+elif idade >= 65 and imc>= 30:
     print(" Classificação nutrcional: Idoso com Obesidade ")
-if idade < 65 and imc<= 18.5:
+elif idade < 65 and imc<= 18.5:
     print(" Classificação nutrcional: Adulto Abaixo do peso ")
-elif idade < 65 and 18.5<imc and imc >24.9:
+elif idade < 65 and imc>=18.5 and imc <=24.9:
     print(" Classificação nutrcional:Adulto em Peso Adequado ")
-elif idade < 65 and imc<= 24.9 and imc >=29.9:
+elif idade < 65 and imc> 24.9 and imc <=29.9:
     print(" Classificação nutrcional: Adulto Sobrepeso ")
-elif idade < 65 and imc<= 30 and imc >= 34.9:
+elif idade < 65 and imc>= 30 and imc <= 34.9:
     print(" Classificação nutrcional: Adulto com Obesidade Grau I ")
-elif idade < 65 and imc<= 35 and imc >= 39.9:
+elif idade < 65 and imc>= 35 and imc <= 39.9:
     print(" Classificação nutrcional: Adulto com Obesidade Grau II ")
-elif idade < 65 and imc<= 40:
+elif idade < 65 and imc>= 40:
     print(" Classificação nutrcional:Adulto com Obesidade Grau III ")
